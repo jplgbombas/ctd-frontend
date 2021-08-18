6bd3f4c11af08a8c69135d9f9f6b1be1cea205de
jplgbombas <88987502+jplgbombas@users.noreply.github.com>
2021-08-18 19:30:33 -0300

    Primeiro commit

---
+++ scriptcotacaodolar.js
@@ -0,0 +1,7 @@
+var nome = prompt('Olá! Qual o seu nome?');
+
+var dolar = prompt(nome + ', quantos dólares você gostaria de cotar?');
+
+var resultado = (dolar * 5.30);
+
+alert(nome + ', hoje US$' + dolar + ' valem R$' + resultado);
